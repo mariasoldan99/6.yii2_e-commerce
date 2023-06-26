a5157d2d9d441b9fd63454fe48e388af330e8881
MariaS <m.soldan99@gmail.com>
2023-06-26 11:35:05 +0300

    Create console script to create admin users

---
+++ console/controllers/AppController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace console\controllers;
+
+use common\models\User;
+use yii\console\Controller;
+use yii\helpers\Console;
+
+class AppController extends Controller
+{
+    public function actionCreateAdminUser($username, $password = null)
+    {
+        $user = new User();
+        $user->firstname = $username;
+        $user->lastname = $username;
+        $user->email = $username.'@example.com';
+        $user->username = $username;
+        $user->admin = 1;
+        $user->status = User::STATUS_ACTIVE;
+        $password = $password ?: \Yii::$app->security->generateRandomString(8);
+        $user->setPassword($password);
+        if($user->save()){
+            Console::output("User has been created");
+            Console::output('Username: '.$username);
+            Console::output('Password: '.$password);
+        }else {
+            Console::error("User \"$username\" was not created");
+        }
+    }
+}
